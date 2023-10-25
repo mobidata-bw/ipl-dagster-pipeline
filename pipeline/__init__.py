e70b58e11dbd36adb9cfeaf9fd8014371735caae
Holger Bruch <hb@mfdz.de>
2023-10-25 18:37:12 +0200

    Add first version of radvis import

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -7,10 +7,11 @@ from dagster import (
     load_assets_from_modules,
 )
 
-from .assets import sharing
+from .assets import radvis, sharing
 from .resources import LamassuResource, PostGISGeoPandasIOManager
+from .resources.gdal import Ogr2OgrResource
 
-assets = load_assets_from_modules([sharing])
+assets = load_assets_from_modules([sharing, radvis])
 
 defs = Definitions(
     assets=assets,
@@ -20,6 +21,13 @@ defs = Definitions(
         'pg_gpd_io_manager': PostGISGeoPandasIOManager(
             host=EnvVar('IPL_POSTGRES_HOST'),
             user=EnvVar('IPL_POSTGRES_USER'),
+            port=EnvVar.int('IPL_POSTGRES_PORT'),
+            password=EnvVar('IPL_POSTGRES_PASSWORD'),
+            database=EnvVar('IPL_POSTGRES_DB'),
+        ),
+        'ogr2ogr': Ogr2OgrResource(
+            host=EnvVar('IPL_POSTGRES_HOST'),
+            username=EnvVar('IPL_POSTGRES_USER'),
             port=EnvVar.int('IPL_POSTGRES_PORT'),
             password=EnvVar('IPL_POSTGRES_PASSWORD'),
             database=EnvVar('IPL_POSTGRES_DB'),
---
+++ pipeline/resources/gdal.py
@@ -0,0 +1,44 @@
+from typing import Optional, Sequence
+
+from dagster import (
+    Config,
+    ConfigurableResource,
+)
+from osgeo import ogr
+from osgeo_utils.samples import ogr2ogr
+
+
+# need mypy to ignore following line due to https://github.com/dagster-io/dagster/issues/17443
+class Ogr2OgrResource(ConfigurableResource):  # type: ignore
+    username: str = 'postgres'
+    password: str = 'postgres'
+    host: str = 'localhost'
+    port: int = 5432
+    database: Optional[str]
+
+    def import_file(self, file_to_import: str, pg_use_copy=True, schema: str = 'public'):
+        connect_string = self._connect_string()
+        schema_param = f'SCHEMA={schema}'
+        args = [
+            'ogr2ogr',
+            '-f',
+            'PostgreSQL',
+            connect_string,
+            '-lco',
+            schema_param,
+            '-overwrite',
+        ]
+
+        if pg_use_copy:
+            args.extend(['--config', 'PG_USE_COPY', 'YES'])
+
+        args.append(file_to_import)
+        ogr.UseExceptions()
+        exit_code = ogr2ogr.main(args)
+        if exit_code != 0:
+            raise Exception(f'Could not import {file_to_import}')
+        return
+
+    def _connect_string(self):
+        database = self.database if self.database else self.username
+        return f'PG:user={self.username} password={self.password} host={self.host} port={self.port} dbname={database}'
