ddfc5a5e6394981199bebf74e991b18ee32ee90e
Holger Bruch <hb@mfdz.de>
2023-11-12 09:44:54 +0100

    replace urlretrieve by requests, handle user-agent and last-modified

--- pipeline/util/urllib.py
+++ pipeline/util/urllib.py
@@ -2,10 +2,22 @@ import gzip
 import os
 import shutil
 import tempfile
-from urllib.request import urlretrieve
+from datetime import datetime
+from email.utils import parsedate_to_datetime
+
+import requests
+
+user_agent = 'IPL (MobiData-BW) +https://github.com/mobidata-bw/ipl-dagster-pipeline'
 
 
-def download(source: str, destination_path: str, filename: str, create_precompressed: bool = False) -> None:
+def download(
+    source: str,
+    destination_path: str,
+    filename: str,
+    create_precompressed: bool = False,
+    force: bool = False,
+    timeout=15,
+) -> None:
     """
     Downloads given source and, after completion, moves it to destination
     path with the specified name. Pre-existings files of the same name will
@@ -15,6 +27,7 @@ def download(source: str, destination_path: str, filename: str, create_precompre
 
     Note: timestamps of remote files are not retained.
     """
+
     with tempfile.TemporaryDirectory() as tmpdirname:
         tmpfilename = os.path.join(tmpdirname, filename)
         finalfilename = os.path.join(destination_path, filename)
@@ -22,14 +35,32 @@ def download(source: str, destination_path: str, filename: str, create_precompre
         if not os.path.exists(destination_path):
             os.makedirs(destination_path)
 
-        if source.startswith(('http:', 'https:')):
-            urlretrieve(source, tmpfilename)  # noqa: S310 nosec explicitly checked for http
-        else:
-            raise ValueError from None
+        headers = {'User-Agent': user_agent}
+        if not force and os.path.exists(finalfilename):
+            pre_existing_file_last_modified = datetime.utcfromtimestamp(os.path.getmtime(finalfilename))
+            headers['If-Modified-Since'] = pre_existing_file_last_modified.strftime('%a, %d %b %Y %H:%M:%S GMT')
+
+        response = requests.get(source, headers=headers, stream=True, timeout=timeout)
+        if response.status_code == 304:
+            # File not modified since last download
+            return
+
+        with open(tmpfilename, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:
+                    file.write(chunk)
+
+        last_modified = None
+        last_modified_str = response.headers.get('Last-Modified')
+        if last_modified_str:
+            last_modified = parsedate_to_datetime(last_modified_str)
+            os.utime(tmpfilename, (last_modified.timestamp(), last_modified.timestamp()))
 
         if gzip:
             with open(tmpfilename, 'rb') as f_in, gzip.open(tmpfilename + '.gz', 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
+                if last_modified:
+                    os.utime(tmpfilename + '.gz', (last_modified.timestamp(), last_modified.timestamp()))
                 os.replace(tmpfilename + '.gz', finalfilename + '.gz')
 
         os.replace(tmpfilename, finalfilename)
