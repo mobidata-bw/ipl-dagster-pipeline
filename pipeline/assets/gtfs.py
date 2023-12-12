03152925cb0ea6b4ca4bb22af9b91ea476e378f0
Holger Bruch <hb@mfdz.de>
2023-12-12 13:47:17 +0100

    env variable changes

--- pipeline/assets/gtfs.py
+++ pipeline/assets/gtfs.py
@@ -14,25 +14,25 @@ from dagster_docker import docker_container_op
 import_op = docker_container_op.configured(
     {
         'image': {'env': 'IPL_GTFS_IMPORTER_IMAGE'},
-        'networks': ['ipl_ipl'],
+        'networks': [os.getenv('IPL_GTFS_IMPORTER_NETWORK')],
         'env_vars': [
-            'PGHOST=gtfs-db',
+            'PGHOST=' + os.getenv('IPL_GTFS_DB_POSTGRES_HOST'),
             'PGUSER=' + os.getenv('IPL_GTFS_DB_POSTGRES_USER'),
             'PGPASSWORD=' + os.getenv('IPL_GTFS_DB_POSTGRES_PASSWORD'),
             'PGDATABASE=' + os.getenv('IPL_GTFS_DB_POSTGRES_DB'),
-            'GTFS_DOWNLOAD_URL',  # no var name mapping required
+            'GTFS_DOWNLOAD_URL=' + os.getenv('IPL_GTFS_IMPORTER_GTFS_DOWNLOAD_URL'),
+            'GTFS_DOWNLOAD_USER_AGENT=' + os.getenv('IPL_GTFS_IMPORTER_GTFS_DOWNLOAD_USER_AGENT'),
             'GTFS_IMPORTER_DB_PREFIX=' + os.getenv('IPL_GTFS_DB_POSTGRES_DB_PREFIX'),
             'GTFS_IMPORTER_DSN_FILE=/var/gtfs/pgbouncer-dsn.txt',
             'GTFS_TMP_DIR=/var/gtfs',
             'POSTGREST_USER=' + os.getenv('IPL_GTFS_DB_POSTGREST_USER'),
             'POSTGREST_PASSWORD=' + os.getenv('IPL_GTFS_DB_POSTGREST_PASSWORD'),
-            'GTFS_DOWNLOAD_USER_AGENT',
         ],
         'container_kwargs': {
             #   "auto_remove": True # auto_remove currently results in error
             'volumes': [
-                os.path.join(os.getenv('IPL_ROOT_DIR'), 'var/gtfs/:/var/gtfs/:rw'),
-                os.path.join(os.getenv('IPL_ROOT_DIR'), 'etc/gtfs:/etc/gtfs'),
+                os.path.join(os.getenv('IPL_GTFS_IMPORTER_HOST_GTFS_OUTPUT_DIR'), ':/var/gtfs/:rw'),
+                os.path.join(os.getenv('IPL_GTFS_IMPORTER_HOST_CUSTOM_SCRIPTS_DIR'), ':/etc/gtfs'),
             ]
         },
     },
