2e7842870a9ffb497341bdaf9b2fea78826eb00a
Ernesto Ruge <ernesto.ruge@binary-butterfly.de>
2025-03-03 08:09:14 +0100

    clearification
    Co-authored-by: Jannis R <mail@jannisr.de>

--- scripts/download_webcams.sh
+++ scripts/download_webcams.sh
@@ -8,7 +8,7 @@ set -x
 find /var/webcam -mtime +$IPL_WEBCAM_KEEP_DAYS -type f -delete
 # Delete all empty directories
 find /var/webcam  -type d -empty -delete
-# Download all new files. Limit to now -(DOWNLOAD_MAX_DAYS - 1) days, otherwise we would delete and re-download data
+# Download all new files. Limit to now -(IPL_WEBCAM_KEEP_DAYS - 1) days, otherwise we would delete and re-download data
 # all the time.
 DOWNLOAD_MAX_DAYS=$((IPL_WEBCAM_KEEP_DAYS-1))
 lftp -e "mirror --newer-than=now-${DOWNLOAD_MAX_DAYS}days -c --parallel=$IPL_WEBCAM_WORKER --verbose / /var/webcam; quit;" -u $IPL_WEBCAM_USER,$IPL_WEBCAM_PASSWORD $IPL_WEBCAM_SERVER
