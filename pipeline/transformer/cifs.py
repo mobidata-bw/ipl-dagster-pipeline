c1c61eb428f1802bc6d334fabcdabc6a5d0a7dd1
Holger Bruch <hb@mfdz.de>
2026-05-05 00:19:17 +0200

    fix: provide unique ids for multi-period incidents

--- pipeline/transformer/cifs.py
+++ pipeline/transformer/cifs.py
@@ -364,8 +364,15 @@ class DatexII2CifsTransformer:
                 closure['location'] = location
 
                 periods = self._get_periods(situationRecord)
+                period_counter = 0
                 for period in periods:
                     new_closure = closure.copy()
+                    # Append couter to create unique IDs though roadworks have been split
+                    # Note: we increase counter before checking if period should be skipped
+                    # which will result in (somewhat more) stable IDs if roadworks periods 
+                    # are unmodified 
+                    period_counter += 1
+                    new_closure['id']=f'{new_closure['id']}.{period_counter:03d}'
                     (starttime, endtime) = period
                     if self.current_time.astimezone() > datetime.fromisoformat(endtime):
                         # ignore periods in the past
