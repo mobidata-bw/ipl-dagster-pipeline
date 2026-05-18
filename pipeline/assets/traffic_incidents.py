0275e8b166dc1bdce67af5aa71a16a07d4188653
Holger Bruch <hb@mfdz.de>
2026-05-18 22:16:34 +0200

    fix: fix naming inconsistencies

--- pipeline/assets/traffic_incidents.py
+++ pipeline/assets/traffic_incidents.py
@@ -27,13 +27,13 @@ WEB_ROOT = os.getenv('WWW_ROOT_DIR', './tmp/www')
 IPL_MOBILITHEK_CERT_PATH = os.getenv('IPL_MOBILITHEK_CERT_PATH', '')
 IPL_MOBILITHEK_KEY_PATH = os.getenv('IPL_MOBILITHEK_KEY_PATH', '')
 
-ROADWORKS_DATEX2_DOWNLOAD_URL = os.getenv('ROADWORKS_SVZBW_DATEX2_DOWNLOAD_URL', '')
+ROADWORKS_SVZBW_DATEX2_DOWNLOAD_URL = os.getenv('ROADWORKS_SVZBW_DATEX2_DOWNLOAD_URL', '')
 ROADWORKS_ADB_SHORT_TERM_DATEX2_DOWNLOAD_URL = os.getenv('IPL_ROADWORKS_ADB_SHORT_TERM_DATEX2_DOWNLOAD_URL', '')
 ROADWORKS_ADB_LONG_TERM_DATEX2_DOWNLOAD_URL = os.getenv('IPL_ROADWORKS_ADB_LONG_TERM_DATEX2_DOWNLOAD_URL', '')
 INCIDENTS_LMSBW_DATEX2_DOWNLOAD_URL = os.getenv('IPL_INCIDENTS_LMSBW_DATEX2_DOWNLOAD_URL', '')
 ROADWORKS_ADB_SHORT_TERM_DATEX2_FIILENAME = 'roadworks_adb_short_term.datex2.xml'
 ROADWORKS_ADB_LONG_TERM_DATEX2_FIILENAME = 'roadworks_adb_long_term.datex2.xml'
-ROADWORKS_DATEX2_FILENAME = 'roadworks_svzbw.datex2.xml'
+ROADWORKS_SVZBW_DATEX2_FILENAME = 'roadworks_svzbw.datex2.xml'
 INCIDENTS_LMSBW_DATEX2_FIILENAME = 'incidents_lmsbw.datex2.xml'
 ROADWORKS_ASSET_KEY_PREFIX = ['traffic', 'roadworks']
 INCIDENTS_ASSET_KEY_PREFIX = ['traffic', 'incidents']
@@ -49,7 +49,7 @@ logger = logging.getLogger(__name__)
     ),
     key_prefix=INCIDENTS_ASSET_KEY_PREFIX,
 )
-def INCIDENTS_LMSBW_datex2() -> None:
+def incidents_lmsbw_datex2() -> None:
     """
     Downloads incidents from Mobilithek and republishes this DATEX2 dataset.
     """
@@ -136,7 +136,12 @@ def roadworks_svzbw_datex2() -> None:
     """
     # Download and republish, if changed
     destination_folder = os.path.join(WEB_ROOT, *ROADWORKS_ASSET_KEY_PREFIX)
-    download(ROADWORKS_DATEX2_DOWNLOAD_URL, destination_folder, ROADWORKS_DATEX2_FILENAME, create_precompressed=True)
+    download(
+        ROADWORKS_SVZBW_DATEX2_DOWNLOAD_URL,
+        destination_folder,
+        ROADWORKS_SVZBW_DATEX2_FILENAME,
+        create_precompressed=True,
+    )
 
 
 @asset(
@@ -152,7 +157,7 @@ def roadworks_cifs() -> dict:
     """
     Transforms roadworks datasets into waze cifs format and publishes them.
     """
-    source = os.path.join(WEB_ROOT, *ROADWORKS_ASSET_KEY_PREFIX, ROADWORKS_DATEX2_FILENAME)
+    source = os.path.join(WEB_ROOT, *ROADWORKS_ASSET_KEY_PREFIX, ROADWORKS_SVZBW_DATEX2_FILENAME)
     # TODO extend here if further roadwork sources are addedd
     return DatexII2CifsTransformer('MobiData BW').transform(source, 'cifs')
 
@@ -172,7 +177,7 @@ def roadworks_geojson() -> dict:
     Note: these may include roadworks with geometry type point. A point geometry type
     is not recommended as downstream standards like e.g. CIFS can't handle them.
     """
-    source = os.path.join(WEB_ROOT, *ROADWORKS_ASSET_KEY_PREFIX, ROADWORKS_DATEX2_FILENAME)
+    source = os.path.join(WEB_ROOT, *ROADWORKS_ASSET_KEY_PREFIX, ROADWORKS_SVZBW_DATEX2_FILENAME)
     return DatexII2CifsTransformer('MobiData BW').transform(source, 'geojson')
 
 
